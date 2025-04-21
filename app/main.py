255dcfd33bbe174e522b328d92d90ac938adf3c1
Kyrylo Fursov <kirfurs31@gmail.com>
2025-04-21 23:13:56 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from typing import Callable
+from functools import wraps
+
+unique_calls = {}
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> int:
+        func_call = (func, args, tuple(sorted(kwargs.items())))
+
+        if func_call in unique_calls:
+            print("Getting from cache")
+            return unique_calls[func_call]
+        print("Calculating new result")
+        unique_calls[func_call] = func(*args, **kwargs)
+
+        return unique_calls[func_call]
+
+    return wrapper
